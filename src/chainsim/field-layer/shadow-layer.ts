6bcaffb95515ee10f7731d8c559d9a21fadcf13d
Phong Tran <s2@puyonexus.com>
2020-08-19 08:57:15 -0400

    Update shadow-layer.ts

--- src/chainsim/field-layer/shadow-layer.ts
+++ src/chainsim/field-layer/shadow-layer.ts
@@ -6,6 +6,7 @@ import { colorPop } from '../animation/color-pop';
 import { garbagePop } from '../animation/garbage-pop';
 import { getSplitGroups, getStaggerValues } from '../animation/popping-stagger';
 import { StateContainer } from '../container';
+import { PuyoField } from '../../solver/field';
 
 class ShadowLayer extends Layer {
   constructor(parent: StateContainer, fieldSettings: LayerSettings) {
@@ -18,18 +19,34 @@ class ShadowLayer extends Layer {
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.cols; c++) {
         const i = r * this.cols + c;
-        this.sprites[i] = new PIXI.Sprite(this.puyoTextures[`none_0.png`]);
+        this.sprites[i] = new PIXI.Sprite(this.puyoTextures[`red_0.png`]);
         const sprite = this.sprites[i];
 
         sprite.anchor.set(0.5);
         const { x, y } = this.cellPos.get(r, c);
         sprite.x = x;
         sprite.y = y;
+        sprite.alpha = 0.5;
         sprite.interactive = false;
 
         this.addChild(sprite);
       }
     }
+  }
+
+  public refreshSprites(field: PuyoField): void {
+    for (let r = 0; r < this.rows; r++) {
+      for (let c = 0; c < this.cols; c++) {
+        const puyo = field.get(r, c);
+        const name = PUYONAME[puyo];
+        const sprite = this.sprites[r * this.cols + c];
+        sprite.texture = this.puyoTextures[`${name}_0.png`];
+      }
+    }
+  }
+
+  public update(delta: number): void {
+    //
   }
 }
 
